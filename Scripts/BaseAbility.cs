c95fe4eb8a55b35074a3acf8ef12641ab7ce7461
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-09-16 14:52:21 +1000

    Created BaseAbility Class.

---
+++ Scripts/BaseAbility.cs
@@ -0,0 +1,29 @@
+﻿/*-------------------------------------------------------------------*
+|  BaseAbility
+|
+|  Author:			Thomas Maltezos
+| 
+|  Description:		Base class for all abilities.
+*-------------------------------------------------------------------*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAbility : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
