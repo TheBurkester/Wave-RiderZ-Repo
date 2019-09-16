e3bd4f73cf794136fc04b61caf748bea7d54445d
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-09-16 14:35:08 +1000

    Created ObjectPool class.

--- Scripts/ObjectPool.cs
+++ Scripts/ObjectPool.cs
@@ -1,26 +1,50 @@
 ﻿/*-------------------------------------------------------------------*
 |  ObjectPool
 |
-|  Author:			
+|  Author:			Thomas Maltezos
 | 
-|  Description:		
+|  Description:		Pool that stores gameObjects that are 'deleted'
+|					often or to only allow a certain amount of
+|					objects.
 *-------------------------------------------------------------------*/
 
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static objectPool sharedInstance;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+    // Creates a shared Instance of the class.
+    void Awake()
     {
-        
+        sharedInstance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Creates a list of objects with the size depending on the amountToPool input.
+    void Start()
     {
-        
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
     }
-}
+    
+    // Returns the pooledObject number.
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
