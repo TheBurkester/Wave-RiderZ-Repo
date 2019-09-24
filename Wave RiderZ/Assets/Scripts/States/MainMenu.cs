b3d532a519068faf41666463a831000333d2e1f3
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-09-24 15:08:31 +1000

    Fixed the class name.

--- Wave RiderZ/Assets/Scripts/States/MainMenu.cs
+++ Wave RiderZ/Assets/Scripts/States/MainMenu.cs
@@ -10,7 +10,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     void Start()
     {
