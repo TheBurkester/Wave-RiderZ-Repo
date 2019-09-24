2c2f67b7d334418b691e907946c13b236b82c659
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-09-24 12:14:49 +1000

    Updated all controllers and objects from prototype.

--- Scripts/Controllers/CameraController.cs
+++ Scripts/Controllers/CameraController.cs
@@ -1,24 +1,30 @@
-﻿/*-------------------------------------------------------------------*
-|  CameraController
-|
-|  Author:			
-| 
-|  Description:		
-*-------------------------------------------------------------------*/
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
+	private float m_speed = 5;
+
+    private Rigidbody rb = null;
+
+    // Start is called before the first frame update
+    void Awake()
+	{
+		GameObject vehicle = GameObject.Find("Vehicle");
+		if (vehicle != null)
+			m_speed = vehicle.GetComponent<VehicleMovement>().speed;
+
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
-        
+	// Update is called once per frame
+	void Update()
+	{
+        Vector3 newPos = rb.position + new Vector3(m_speed * Time.deltaTime, 0, 0);
+        rb.MovePosition(newPos);	//Keep in mind this doesn't update the position until the end of the frame
+
+        //Vector3 newPos = transform.position + new Vector3(m_speed * Time.deltaTime, 0, 0);
+        //transform.Translate(newPos);
     }
 }
--- Scripts/Controllers/SkierController.cs
+++ Scripts/Controllers/SkierController.cs
@@ -1,26 +1,30 @@
-﻿/*-------------------------------------------------------------------*
-|  SkierController
-|
-|  Author:			
-| 
-|  Description:		
-*-------------------------------------------------------------------*/
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+	public float speed = 5;
+    public KeyCode MoveLeft;
+    public KeyCode MoveRight;
+
+	private Rigidbody rb = null;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
+		rb = GetComponent<Rigidbody>();
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (rb != null)
+		{
+            if (Input.GetKey(MoveLeft))
+                rb.AddForce(0, 0, speed);
+			if (Input.GetKey(MoveRight))
+				rb.AddForce(0, 0, -speed);
+		}
+	}
 }
