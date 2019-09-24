679f50b81d49ec3148a27941629172185ae80d08
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-09-24 12:12:18 +1000

    Added Bump Script from Prototype.

---
+++ Scripts/Controllers/Bump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bump : MonoBehaviour
+{
+	public float Force = 5000;
+
+
+	public Rigidbody ObjectToBePushed;
+
+	private Rigidbody rb = null;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			ObjectToBePushed.AddForce(0, 0, -Force);
+			rb.AddForce(0, 0, Force / 2);
+		}
+	}
+}
