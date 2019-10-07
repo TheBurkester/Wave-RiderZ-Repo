1db3634c260e717fe0bd472dd750b085519860af
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-10-07 12:02:41 +1100

    Added BeachBall prefab and added explosion force.

--- Wave RiderZ/Assets/Scripts/Abilities/BeachBallAbility.cs
+++ Wave RiderZ/Assets/Scripts/Abilities/BeachBallAbility.cs
@@ -18,62 +18,97 @@ public class BeachBallAbility : MonoBehaviour
     public KeyCode Left = KeyCode.A;
     public KeyCode Right = KeyCode.D;
     public KeyCode Aim = KeyCode.Space;
+    public KeyCode Shoot = KeyCode.G;
     public float Cooldown = 5.0f; // Replace with timer script functions at a later date.
     public float targetMovementSpeed = 10.0f; // Target's movement speed when aiming.
 
-    private MeshRenderer targetMesh; // Target's Mesh.
+    public static MeshRenderer targetMesh; // Target's Mesh.
     private Rigidbody targetRB; // Target's Rigidbody.
     private float targetPlaneRelation = 5.0f; // Moves the target along with the plane and camera. KEEP VARIABLE THE SAME AS PLANE SPEED IN PLANE CONTROLLER.
+    private GameObject prefab; // Beachball prefab.
+    public static bool isShooting = false; // Has the player pressed the shoot button?
 
     void Awake()
     {
         targetRB = gameObject.GetComponent<Rigidbody>();
         targetMesh = gameObject.GetComponent<MeshRenderer>();
         targetMesh.enabled = false; // Target's mesh is disabled on startup.
+
+        prefab = Resources.Load("BeachBall") as GameObject;
     }
 
     void Update()
     {
-        Vector3 v3PlaneRelation = targetRB.position + new Vector3(targetPlaneRelation * Time.deltaTime, 0, 0); // Target's movement forward in relation to the plane.
-        targetRB.transform.position = v3PlaneRelation;
+        aimTarget();
+    }
 
+    void aimTarget()
+    {
+        // Target's movement forward in relation to the plane.
+        Vector3 v3PlaneRelation = targetRB.position + new Vector3(targetPlaneRelation * Time.deltaTime, 0, 0);
+        targetRB.transform.position = v3PlaneRelation;
 
         Vector3 v3 = new Vector3();
 
         /*===========================================================================================*/
         /*      Leave this outside of the aim If statement. Won't be called if inside.               */
 
-        if (Input.GetKey(Left) && targetMesh.enabled) // Movement Left.
+        if (!isShooting)
         {
-            v3 += Vector3.forward; // Moving Left = Vector.forward due to scene direction.
-        }
+            if (Input.GetKey(Left) && targetMesh.enabled) // Movement Left.
+            {
+                v3 += Vector3.forward; // Moving Left = Vector.forward due to scene direction.
+            }
 
-        if (Input.GetKey(Right) && targetMesh.enabled) // Movement Right.
-        {
-            v3 += Vector3.back; // Moving Right = Vector.back due to scene direction.
-        }
+            if (Input.GetKey(Right) && targetMesh.enabled) // Movement Right.
+            {
+                v3 += Vector3.back; // Moving Right = Vector.back due to scene direction.
+            }
 
-        if (Input.GetKey(Up) && targetMesh.enabled) // Movement Up.
-        {
-            v3 += Vector3.right; // Moving Up = Vector.right due to scene direction.
-        }
+            if (Input.GetKey(Up) && targetMesh.enabled) // Movement Up.
+            {
+                v3 += Vector3.right; // Moving Up = Vector.right due to scene direction.
+            }
 
-        if (Input.GetKey(Down) && targetMesh.enabled) // Movement Down.
-        {
-            v3 += Vector3.left; // Moving Down = Vector.left due to scene direction.
+            if (Input.GetKey(Down) && targetMesh.enabled) // Movement Down.
+            {
+                v3 += Vector3.left; // Moving Down = Vector.left due to scene direction.
+            }
         }
-
+        // Simple transform which combines all directions and allows diagonal movement.
         targetRB.transform.Translate(targetMovementSpeed * v3.normalized * Time.deltaTime);
+
+        if (Input.GetKey(Shoot) && targetMesh.enabled)
+        {
+            isShooting = true;
+            shootBall(); // Shoots the beachball.
+            targetMesh.enabled = true;
+        }
         /*===========================================================================================*/
 
-        if (Input.GetKeyDown(Aim))
+        if (Input.GetKeyDown(Aim) || isShooting)
         {
             targetMesh.enabled = true; // Activate target's mesh when aiming.
-
         }
         else if (Input.GetKeyUp(Aim))
         {
             targetMesh.enabled = false; // Disable target's mesh when not aiming.
+        }
+    }
+
+    void shootBall()
+    {
+        // This will break if more than one object pool is used, will need to change the class.
+        GameObject BeachBall = ObjectPool.sharedInstance.GetPooledObject();
+        if (BeachBall != null)
+        {
+            // Landing position travels with the plane. Also keeps the ball from spawning within the camera's view.
+            Vector3 v3LandingPos = targetRB.position + new Vector3(targetPlaneRelation * Time.deltaTime, 20, 0);
+            BeachBall.transform.position = v3LandingPos;
+
+            Rigidbody rb = BeachBall.GetComponent<Rigidbody>();
+            rb.velocity = BeachBall.transform.right * 4; // Keeps the velocity inline with the plane's movement forward.
+            BeachBall.SetActive(true);
         }
     }
 }
---
+++ Wave RiderZ/Assets/Scripts/Object Related/BeachBall.cs
@@ -0,0 +1,58 @@
+﻿/*-------------------------------------------------------------------*
+|  Title:			BeachBall
+|
+|  Author:		    Thomas Maltezos
+| 
+|  Description:		Handles the beach ball's collision.
+*-------------------------------------------------------------------*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachBall : BeachBallAbility
+{
+    public float radius = 5.0f;
+    public float power = 10.0f;
+
+    private Rigidbody rb;
+    private Vector3 reset;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        reset = new Vector3(0, 0, 0); // Basic reset vector.
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("River")) // Will be called if collision with the river occurs.
+        {
+            Vector3 explosionPos = transform.position; // explosion will occur at the impact site.
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius); // List of colliders within the radius.
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>(); // Will get the rigidbodies within the radius.
+
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(power, explosionPos, radius); // Will force each rigidbody away from the origin.
+                }
+            }
+
+            gameObject.SetActive(false); // Deactivates the beachball.
+            rb.velocity = reset; // Resets velocity.
+            rb.angularVelocity = reset; // Resets angular velocity.
+            rb.transform.rotation = Quaternion.Euler(reset); // Resets rotation.
+
+            isShooting = false; // Player isn't shooting anymore.
+            targetMesh.enabled = false; // Disabled target's mesh.
+        }
+    }
+}
