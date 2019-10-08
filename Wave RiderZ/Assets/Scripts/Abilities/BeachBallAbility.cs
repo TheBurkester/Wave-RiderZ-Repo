f5ea2d26d40d9c990cfc539d757e391f219d5c08
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-10-08 11:44:20 +1100

    Removed BaseAbility and implemented cooldown timer.

--- Wave RiderZ/Assets/Scripts/Abilities/BeachBallAbility.cs
+++ Wave RiderZ/Assets/Scripts/Abilities/BeachBallAbility.cs
@@ -17,89 +17,98 @@ public class BeachBallAbility : MonoBehaviour
     public KeyCode Right = KeyCode.D;
     public KeyCode Aim = KeyCode.Space;
     public KeyCode Shoot = KeyCode.G;
-    public float Cooldown = 5.0f; // Replace with timer script functions at a later date.
+    public float Cooldown = 5.0f; // Used to define the length of the ability's cooldown.
     public float targetMovementSpeed = 10.0f; // Target's movement speed when aiming.
     public float riverClampHorizontal = 14; // Editable horizontal clamp.
     public float riverClampForward = 3.5f; // Editable forward clamp.
     public float riverClampBehind = 5; // Editable behind clamp.
     public Rigidbody planeRB = null;
 
-    private MeshRenderer targetMesh; // Target's Mesh.
-    private bool isShooting = false; // Has the player pressed the shoot button.
-    private float riverClampForwardAlter; // Clamp will always be moving forward.
-    private float riverClampBehindAlter; // Clamp will always be moving forward.
-    private Rigidbody targetRB; // Target's Rigidbody.
-    private float targetPlaneRelation = 5.0f; // Moves the target along with the plane and camera. KEEP VARIABLE THE SAME AS PLANE SPEED IN PLANE CONTROLLER.
-    private GameObject prefab; // Beachball prefab.
+    private float m_targetPlaneRelation = 5.0f; // Moves the target along with the plane and camera. KEEP VARIABLE THE SAME AS PLANE SPEED IN PLANE CONTROLLER.
+    private bool m_isShooting = false; // Has the player pressed the shoot button.
+    private float m_riverClampForwardAlter; // Clamp will always be moving forward.
+    private float m_riverClampBehindAlter; // Clamp will always be moving forward.
+    private MeshRenderer m_targetMesh; // Target's Mesh.
+    private Rigidbody m_targetRB; // Target's Rigidbody.
+    private GameObject m_prefab; // Beachball prefab.
+    private Timer m_abilityCooldown; // Timer used for the cooldown.
 
     void Awake()
     {
-        targetRB = gameObject.GetComponent<Rigidbody>();
-        targetMesh = gameObject.GetComponent<MeshRenderer>();
-        targetMesh.enabled = false; // Target's mesh is disabled on startup.
+        m_targetRB = gameObject.GetComponent<Rigidbody>();
+        m_targetMesh = gameObject.GetComponent<MeshRenderer>();
+        m_targetMesh.enabled = false; // Target's mesh is disabled on startup.
+        m_prefab = Resources.Load("BeachBall") as GameObject;
 
-        prefab = Resources.Load("BeachBall") as GameObject;
+        m_abilityCooldown = gameObject.AddComponent<Timer>();
+        m_abilityCooldown.maxTime = Cooldown;
+    }
+
+    void Start()
+    {
+        m_abilityCooldown.SetTimer();
     }
 
     void Update()
     {
-        aimTarget();
+            aimTarget();
     }
 
     void aimTarget()
     {
         // Target's movement forward in relation to the plane.
-        Vector3 v3PlaneRelation = targetRB.position + new Vector3(targetPlaneRelation * Time.deltaTime, 0, 0);
-        targetRB.transform.position = v3PlaneRelation;
+        Vector3 v3PlaneRelation = m_targetRB.position + new Vector3(m_targetPlaneRelation * Time.deltaTime, 0, 0);
+        m_targetRB.transform.position = v3PlaneRelation;
 
-        riverClampForwardAlter = planeRB.position.x - riverClampForward;
-        riverClampBehindAlter = planeRB.position.x - riverClampBehind * 2.5f;
+        m_riverClampForwardAlter = planeRB.position.x - riverClampForward;
+        m_riverClampBehindAlter = planeRB.position.x - riverClampBehind * 2.5f;
 
         Vector3 v3 = new Vector3();
 
         /*===========================================================================================*/
         /*      Leave this outside of the aim If statement. Won't be called if inside.               */
 
-        if (!isShooting)
+        if (!m_isShooting)
         {
-            if (Input.GetKey(Left) && targetMesh.enabled && targetRB.position.z < riverClampHorizontal) // Movement Left.
+            if (Input.GetKey(Left) && m_targetMesh.enabled && m_targetRB.position.z < riverClampHorizontal) // Movement Left.
             {
                 v3 += Vector3.forward; // Moving Left = Vector.forward due to scene direction.
             }
 
-            if (Input.GetKey(Right) && targetMesh.enabled && targetRB.position.z > -riverClampHorizontal) // Movement Right.
+            if (Input.GetKey(Right) && m_targetMesh.enabled && m_targetRB.position.z > -riverClampHorizontal) // Movement Right.
             {
                 v3 += Vector3.back; // Moving Right = Vector.back due to scene direction.
             }
 
-            if (Input.GetKey(Up) && targetMesh.enabled && targetRB.position.x < riverClampForwardAlter) // Movement Up.
+            if (Input.GetKey(Up) && m_targetMesh.enabled && m_targetRB.position.x < m_riverClampForwardAlter) // Movement Up.
             {
                 v3 += Vector3.right; // Moving Up = Vector.right due to scene direction.
             }
 
-            if (Input.GetKey(Down) && targetMesh.enabled && targetRB.position.x > riverClampBehindAlter) // Movement Down.
+            if (Input.GetKey(Down) && m_targetMesh.enabled && m_targetRB.position.x > m_riverClampBehindAlter) // Movement Down.
             {
                 v3 += Vector3.left; // Moving Down = Vector.left due to scene direction.
             }
         }
             // Simple transform which combines all directions and allows diagonal movement.
-            targetRB.transform.Translate(targetMovementSpeed * v3.normalized * Time.deltaTime);
+            m_targetRB.transform.Translate(targetMovementSpeed * v3.normalized * Time.deltaTime);
 
-        if (Input.GetKey(Shoot) && targetMesh.enabled)
+        if (Input.GetKey(Shoot) && m_targetMesh.enabled)
         {
-            isShooting = true;
+            toggleIsShooting(true);
             shootBall(); // Shoots the beachball.
-            targetMesh.enabled = true;
+            toggleMeshEnable(true);
+            m_abilityCooldown.SetTimer();
         }
         /*===========================================================================================*/
 
-        if (Input.GetKeyDown(Aim) || isShooting)
+        if (Input.GetKeyDown(Aim) && !m_abilityCooldown.UnderMax() || m_isShooting)
         {
-            targetMesh.enabled = true; // Activate target's mesh when aiming.
+            m_targetMesh.enabled = true; // Activate target's mesh when aiming.
         }
         else if (Input.GetKeyUp(Aim))
         {
-            targetMesh.enabled = false; // Disable target's mesh when not aiming.
+            m_targetMesh.enabled = false; // Disable target's mesh when not aiming.
         }
     }
 
@@ -110,7 +119,7 @@ public class BeachBallAbility : MonoBehaviour
         if (BeachBall != null)
         {
             // Landing position travels with the plane. Also keeps the ball from spawning within the camera's view.
-            Vector3 v3LandingPos = targetRB.position + new Vector3(targetPlaneRelation * Time.deltaTime, 20, 0);
+            Vector3 v3LandingPos = m_targetRB.position + new Vector3(m_targetPlaneRelation * Time.deltaTime, 20, 0);
             BeachBall.transform.position = v3LandingPos;
 
             Rigidbody rb = BeachBall.GetComponent<Rigidbody>();
@@ -122,11 +131,11 @@ public class BeachBallAbility : MonoBehaviour
     // Used in Beachball script.
     public void toggleIsShooting(bool shooting)
     {
-        isShooting = shooting;
+        m_isShooting = shooting;
     }
     // Used in Beachball script.
     public void toggleMeshEnable(bool enable)
     {
-        targetMesh.enabled = enable;
+        m_targetMesh.enabled = enable;
     }
 }
--- Wave RiderZ/Assets/Scripts/Abilities/TetheredMineAbility.cs
+++ Wave RiderZ/Assets/Scripts/Abilities/TetheredMineAbility.cs
@@ -10,7 +10,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TetheredMineAbility : BaseAbility
+public class TetheredMineAbility : MonoBehaviour
 {
     void Awake()
     {
