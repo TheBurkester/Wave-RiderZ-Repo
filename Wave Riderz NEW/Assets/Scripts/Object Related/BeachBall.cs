ec3bda2fe4960cc150f70d637374e785d652a4ef
SethJPistol <47731608+SethJPistol@users.noreply.github.com>
2019-10-19 12:45:10 +1100

    Code clean

--- Wave Riderz NEW/Assets/Scripts/Object Related/BeachBall.cs
+++ Wave Riderz NEW/Assets/Scripts/Object Related/BeachBall.cs
@@ -1,7 +1,7 @@
 ﻿/*-------------------------------------------------------------------*
 |  Title:			BeachBall
 |
-|  Author:		    Thomas Maltezos
+|  Author:		    Thomas Maltezos / Seth Johnston
 | 
 |  Description:		Handles the beach ball's collision.
 *-------------------------------------------------------------------*/
@@ -12,59 +12,46 @@ public class BeachBall : MonoBehaviour
 {
     public float radius = 5.0f;
     public float power = 10.0f;
-
-    private GameObject m_Target;
+	
     private BeachBallAbility m_bbAbility;
     private Rigidbody m_rb;
-    private Vector3 m_reset;
 
     void Awake()
     {
         m_rb = gameObject.GetComponent<Rigidbody>();
-        m_reset = new Vector3(0, 0, 0); // Basic reset vector.
 
-        m_Target = GameObject.FindWithTag("Target"); // Will search for the target with the tag.
-        if (m_Target != null)
-        {
-            m_bbAbility = m_Target.GetComponent<BeachBallAbility>(); // Will get the script from the target.
-        }
+        GameObject target = GameObject.FindWithTag("Target"); // Will search for the target with the tag.
+        if (target != null)
+            m_bbAbility = target.GetComponent<BeachBallAbility>(); // Will get the script from the target.
     }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("River")) // Will be called if collision with the river occurs.
         {
-            Vector3 explosionPos = transform.position; // explosion will occur at the impact site.
-			explosionPos.y = 0;		//Make sure that there is no y component
+            Vector3 explosionPos = transform.position;	// explosion will occur at the impact site.
+			explosionPos.y = 0;							//Make sure that there is no y component
             Collider[] colliders = Physics.OverlapSphere(explosionPos, radius); // List of colliders within the radius.
-            foreach (Collider hit in colliders)
+
+            foreach (Collider hit in colliders)										//For all the objects in the radius,
             {
-                //Rigidbody rb = hit.GetComponent<Rigidbody>(); // Will get the rigidbodies within the radius.
-
-                //if (rb != null)
-                //{
-                    //rb.AddExplosionForce(power, explosionPos, radius); // Will force each rigidbody away from the origin.
-
-                //}
-				
-				if (hit.CompareTag("Skier"))
+				if (hit.CompareTag("Skier"))										//If this object is a skier,
 				{
-					Tether otherTether = hit.GetComponent<Tether>();
-					Vector3 distanceToHit = hit.transform.position - explosionPos;
-					distanceToHit.y = 0;    //Make sure there is no y compenent
-											//float distanceToHit.magnitude		//Do a check of distance magnitude and adjust force amount here
-					otherTether.forceToApply += power * distanceToHit.normalized;
-					//otherTether.forceToApply += bonkForce * tether.Direction();
+					Tether tether = hit.GetComponent<Tether>();						//Get their tether
+					Vector3 distanceToHit = hit.transform.position - explosionPos;	//Get the difference in position between the skier and the explosion point
+					distanceToHit.y = 0;											//Make sure there is no y compenent
+					//float distanceToHit.magnitude		*Do a check of distance magnitude and adjust force amount here*
+					tether.forceToApply += power * distanceToHit.normalized;		//Add a force on the skier, pushing away from the explosion point
 				}
             }
 
             gameObject.SetActive(false); // Deactivates the beachball.
-            m_rb.velocity = m_reset; // Resets velocity.
-            m_rb.angularVelocity = m_reset; // Resets angular velocity.
-            m_rb.transform.rotation = Quaternion.Euler(m_reset); // Resets rotation.
+            m_rb.velocity = Vector3.zero; // Resets velocity.
+            m_rb.angularVelocity = Vector3.zero; // Resets angular velocity.
+            m_rb.transform.rotation = Quaternion.Euler(Vector3.zero); // Resets rotation.
 
-            m_bbAbility.toggleIsShooting(false); // Player isn't shooting anymore.
-            m_bbAbility.toggleMeshEnable(false); // Disabled target's mesh.
+            m_bbAbility.ToggleIsShooting(false); // Player isn't shooting anymore.
+            m_bbAbility.ToggleMeshEnable(false); // Disabled target's mesh.
         }
     }
 }
