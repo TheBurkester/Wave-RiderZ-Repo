9c32f869b757f1e96045cb107777c279e6c56f98
SethJPistol <47731608+SethJPistol@users.noreply.github.com>
2019-10-22 16:01:53 +1100

    Added PrefabLoader class, basic prefab loading working

---
+++ Wave Riderz NEW/Assets/Scripts/Object Related/PrefabLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class PrefabLoader : MonoBehaviour
+{
+	private string[] m_prefabFiles;
+
+
+    void Start()
+    {
+		m_prefabFiles = Directory.GetFiles(Application.dataPath + "/Resources", "*.prefab");
+		foreach (string path in m_prefabFiles)
+		{
+			if (File.Exists(path))
+			{
+				Console.WriteLine("Processed file '{0}'.", path);
+			}
+		}
+		//GameObject test = Instantiate(Resources.Load<GameObject>("largeRock")) as GameObject;
+
+		//object test = Resources.Load<GameObject>("") as object;
+
+		GameObject[] prefabs = Resources.LoadAll<GameObject>("Obstacles");
+	}
+}
