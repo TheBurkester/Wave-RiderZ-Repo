15644825117b2d769b354456f09c84c6f175f716
SethJPistol <47731608+SethJPistol@users.noreply.github.com>
2019-10-23 16:43:22 +1100

    Prefab loader implemented and working, 5 obstacle prefabs added

--- Wave Riderz NEW/Assets/Scripts/Object Related/PrefabLoader.cs
+++ Wave Riderz NEW/Assets/Scripts/Object Related/PrefabLoader.cs
@@ -6,23 +6,40 @@ using System;
 
 public class PrefabLoader : MonoBehaviour
 {
-	private string[] m_prefabFiles;
-
+	private GameObject[] m_prefabs;
+	private bool[] m_prefabActive;
+	public PlaneController plane = null;
+	public GameManager gameManager = null;
+	public int numberOfPrefabs = 5;
+	public int forwardsOffset = 20;
 
     void Start()
     {
-		m_prefabFiles = Directory.GetFiles(Application.dataPath + "/Resources", "*.prefab");
-		foreach (string path in m_prefabFiles)
+		m_prefabs = Resources.LoadAll<GameObject>("Obstacles"); //Load all the prefabs in the /Resources/Obstacles folder and store them in an array
+
+		//numberOfPrefabs = plane.forwardSpeed * gameManager.roundTimeLimit;
+
+		Debug.Assert(m_prefabs.Length >= numberOfPrefabs, "Number of required prefabs is larger than the number of available prefabs.");
+
+		m_prefabActive = new bool[m_prefabs.Length];
+
+		for (int i = 0; i < numberOfPrefabs; ++i)
 		{
-			if (File.Exists(path))
+			int prefabToSpawn = UnityEngine.Random.Range(0, m_prefabs.Length);
+			if (m_prefabActive[prefabToSpawn] == false)
 			{
-				Console.WriteLine("Processed file '{0}'.", path);
+				if (m_prefabs[prefabToSpawn] != null)
+				{
+					Vector3 positionAlongRiver = new Vector3(0, 0, 40 * i + forwardsOffset);
+					m_prefabs[prefabToSpawn].transform.position = positionAlongRiver;
+					Instantiate(m_prefabs[prefabToSpawn]);
+					m_prefabActive[prefabToSpawn] = true;
+				}
+			}
+			else
+			{
+				--i;
 			}
 		}
-		//GameObject test = Instantiate(Resources.Load<GameObject>("largeRock")) as GameObject;
-
-		//object test = Resources.Load<GameObject>("") as object;
-
-		GameObject[] prefabs = Resources.LoadAll<GameObject>("Obstacles");
 	}
 }
