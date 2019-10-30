19dfdac643f398a03cb27be0050a85597138b355
SethJPistol <47731608+SethJPistol@users.noreply.github.com>
2019-10-30 11:11:11 +1100

    Fixed tether jittering

--- Wave Riderz NEW/Assets/Scripts/Tether Related/Tether.cs
+++ Wave Riderz NEW/Assets/Scripts/Tether Related/Tether.cs
@@ -74,7 +74,7 @@ public class Tether : MonoBehaviour
 		//Tether physics
 		Vector3 testPosition = transform.position + (testVelocity) * Time.deltaTime;    //Where is the object going to go next?
 		Vector3 testDistance = testPosition - tetherPosition;							//Distance between the new point and the tether point
-		if (testDistance.magnitude > currentLength)										//If the new point is outside the length of the rope,
+		if (testDistance.magnitude > (currentLength - 0.1f))							//If the new point is outside the length of the rope, or a tiny bit shorter,
 			testPosition = tetherPosition + testDistance.normalized * currentLength;	//Pull it back to the rope length in the direction of the rope
 
 		//Set the final values
