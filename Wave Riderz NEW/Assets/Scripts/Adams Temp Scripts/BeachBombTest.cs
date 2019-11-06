1ed054ec495b2c6f7403ebd23bdba9f6e657f762
AdamtheAmazing <48895361+AdamtheAmazing@users.noreply.github.com>
2019-11-06 16:59:44 +1100

    Added new plane,animations, obstacles, tweaked UI and scenes

---
+++ Wave Riderz NEW/Assets/Scripts/Adams Temp Scripts/BeachBombTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachBombTest : MonoBehaviour
+{
+    public Animator PlaneAnim;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            PlaneAnim.SetBool("FireBeachBomb", true);
+            Debug.Log("you got it!");
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            PlaneAnim.SetBool("FireBeachBomb", false);
+            Debug.Log("Im done");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            PlaneAnim.SetBool("OpenMine", true);
+            Debug.Log("All Mine!");
+        }
+        if (Input.GetKey(KeyCode.C))
+        { 
+            PlaneAnim.SetBool("OpenMine", false);
+            Debug.Log("Closed");
+        }
+    }
+}
