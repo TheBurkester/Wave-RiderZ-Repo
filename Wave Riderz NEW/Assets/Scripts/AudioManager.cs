bdb810b9ec830b1b228df6f02b151b36f8a465cd
SirMaxWell <50089794+SirMaxWell@users.noreply.github.com>
2019-11-09 15:46:23 +1100

    added the loop added the warning message

--- Wave Riderz NEW/Assets/Scripts/AudioManager.cs
+++ Wave Riderz NEW/Assets/Scripts/AudioManager.cs
@@ -9,7 +9,7 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
-    // Start is called before the first frame update
+    
     void Awake()
     {
         foreach (Sound s in sounds)
@@ -19,12 +19,20 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
    public void Play (string name )
     {
       Sound s = Array.Find(sounds, sound => sound.name == name);
+        // warning message if it cant find the right sounds 
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+            
         s.source.Play();
     }
 
