690ce171fe342f378503e59f0196f17185ecf3b4
burkester2000 <48895374+burkester2000@users.noreply.github.com>
2019-11-19 15:32:27 +1100

    Added new Particles, New Idle Animation, and new Crocodile Material

--- Wave Riderz NEW/Assets/Scripts/NickAnimationTest.cs
+++ Wave Riderz NEW/Assets/Scripts/NickAnimationTest.cs
@@ -24,46 +24,63 @@ public class NickAnimationTest : MonoBehaviour
 
     void tiltCharacter()
     {
+        //gets references to the left stick input of the controller
         float axisX = XCI.GetAxis(XboxAxis.LeftStickX, controller);
         float axisY = XCI.GetAxis(XboxAxis.LeftStickY, controller);
+
+        //if the control stick is pushed 40% of the way to the right
         if (axisX >= 0.4)
         {
+            //sets the animation state to moving  right
             characterAnim.SetBool("MovingLeft", false);
             characterAnim.SetBool("MovingRight", true);
         }
+        //if the control stick is pushed 40% of the way to the left
         else if (axisX <= -0.4)
         {
+            //sets the animation state to moving left
             characterAnim.SetBool("MovingRight", false);
             characterAnim.SetBool("MovingLeft", true);
         }
+        //if neither
         else
         {
+            //sets the animation state to idle
             characterAnim.SetBool("MovingRight", false);
             characterAnim.SetBool("MovingLeft", false);
         }
+        //if the control stick is pushed 40% of the way upwards
         if (axisY >= 0.4)
         {
+            //sets the animation state to moving forward
             characterAnim.SetBool("MovingForward", true);
             characterAnim.SetBool("MovingBackward", false);
         }
+        //if the control stick is pushed 40% of the way downwards
         else if (axisY <= -0.4)
         {
+            //sets the animation state to moving backwards
             characterAnim.SetBool("MovingForward", false);
             characterAnim.SetBool("MovingBackward", true);
         }
+        //if neither
         else
         {
+            //sets the animation state to idle
             characterAnim.SetBool("MovingForward", false);
             characterAnim.SetBool("MovingBackward", false);
         }
 
     }
+
     void damageCharacter()
     {
+        //if condition is met
        if (Input.GetKeyDown(KeyCode.A))
         {
+            //sets DamageTaken trigger in animator
             characterAnim.SetTrigger("DamageTaken");
-            // characterAnim.SetBool("IsDamaged", false);
+            //reduces current lives counter in animator by 1
             characterAnim.SetInteger("Lives", characterAnim.GetInteger("Lives") - 1); 
 
         }
